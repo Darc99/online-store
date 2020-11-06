53989608ee1de91e75645a681619c9ebef12e166
Darc99 <lennyyhugoh@gmail.com>
2020-11-06 02:08:17 +0100

    generated online-store entity model

---
+++ src/main/java/com/darc/store/repository/OrderItemRepository.java
@@ -0,0 +1,14 @@
+package com.darc.store.repository;
+
+import com.darc.store.domain.OrderItem;
+
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data  repository for the OrderItem entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+}
---
+++ src/main/java/com/darc/store/repository/ProductOrderRepository.java
@@ -0,0 +1,14 @@
+package com.darc.store.repository;
+
+import com.darc.store.domain.ProductOrder;
+
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data  repository for the ProductOrder entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface ProductOrderRepository extends JpaRepository<ProductOrder, Long> {
+}
