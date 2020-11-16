de3114db7957ea4597cdf30d7fa901ec801cc111
Darc99 <lennyyhugoh@gmail.com>
2020-11-17 00:09:33 +0100

    added security logic to the services

--- src/main/java/com/darc/store/service/ShipmentService.java
+++ src/main/java/com/darc/store/service/ShipmentService.java
@@ -2,15 +2,13 @@ package com.darc.store.service;
 
 import com.darc.store.domain.Shipment;
 import com.darc.store.repository.ShipmentRepository;
+import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
 
 /**
  * Service Implementation for managing {@link Shipment}.
@@ -18,58 +16,62 @@ import java.util.Optional;
 @Service
 @Transactional
 public class ShipmentService {
+  private final Logger log = LoggerFactory.getLogger(ShipmentService.class);
 
-    private final Logger log = LoggerFactory.getLogger(ShipmentService.class);
+  private final ShipmentRepository shipmentRepository;
 
-    private final ShipmentRepository shipmentRepository;
+  public ShipmentService(ShipmentRepository shipmentRepository) {
+    this.shipmentRepository = shipmentRepository;
+  }
 
-    public ShipmentService(ShipmentRepository shipmentRepository) {
-        this.shipmentRepository = shipmentRepository;
-    }
+  /**
+   * Save a shipment.
+   *
+   * @param shipment the entity to save.
+   * @return the persisted entity.
+   */
+  public Shipment save(Shipment shipment) {
+    log.debug("Request to save Shipment : {}", shipment);
+    return shipmentRepository.save(shipment);
+  }
 
-    /**
-     * Save a shipment.
-     *
-     * @param shipment the entity to save.
-     * @return the persisted entity.
-     */
-    public Shipment save(Shipment shipment) {
-        log.debug("Request to save Shipment : {}", shipment);
-        return shipmentRepository.save(shipment);
-    }
+  /**
+   * Get all the shipments.
+   *
+   * @param pageable the pagination information.
+   * @return the list of entities.
+   */
+  @Transactional(readOnly = true)
+  public Page<Shipment> findAll(Pageable pageable) {
+    log.debug("Request to get all Shipments");
+    // return shipmentRepository.findAll(pageable);
+    if (SecurityUtils.isCurrentUserInRole(AuthoritiesConstants.ADMIN)) {
+      return shipmentRepository.findAll(pageable);
+    } else return shipmentRepository.findAllByCustomerUserLogin(SecurityUtils.getCurrentUserLogin().get(), pageable);
+  }
 
-    /**
-     * Get all the shipments.
-     *
-     * @param pageable the pagination information.
-     * @return the list of entities.
-     */
-    @Transactional(readOnly = true)
-    public Page<Shipment> findAll(Pageable pageable) {
-        log.debug("Request to get all Shipments");
-        return shipmentRepository.findAll(pageable);
-    }
+  /**
+   * Get one shipment by id.
+   *
+   * @param id the id of the entity.
+   * @return the entity.
+   */
+  @Transactional(readOnly = true)
+  public Optional<Shipment> findOne(Long id) {
+    log.debug("Request to get Shipment : {}", id);
+    // return shipmentRepository.findById(id);
+    if (SecurityUtils.isCurrentUserInRole(AuthoritiesConstants.ADMIN)) {
+      return shipmentRepository.findById(id);
+    } else return shipmentRepository.findOneByIdAndCustomerUserLogin(id, SecurityUtils.getCurrentUserLogin().get());
+  }
 
-
-    /**
-     * Get one shipment by id.
-     *
-     * @param id the id of the entity.
-     * @return the entity.
-     */
-    @Transactional(readOnly = true)
-    public Optional<Shipment> findOne(Long id) {
-        log.debug("Request to get Shipment : {}", id);
-        return shipmentRepository.findById(id);
-    }
-
-    /**
-     * Delete the shipment by id.
-     *
-     * @param id the id of the entity.
-     */
-    public void delete(Long id) {
-        log.debug("Request to delete Shipment : {}", id);
-        shipmentRepository.deleteById(id);
-    }
+  /**
+   * Delete the shipment by id.
+   *
+   * @param id the id of the entity.
+   */
+  public void delete(Long id) {
+    log.debug("Request to delete Shipment : {}", id);
+    shipmentRepository.deleteById(id);
+  }
 }
